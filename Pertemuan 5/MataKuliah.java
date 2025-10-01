371e15580a45a543c4facbbdaf158fcf24b200cc
Farrel Aqilla Novianto <farrel.aqilla.nov@gmail.com>
2025-10-01 17:36:49 +0700

    tugas 5

--- Pertemuan 5/MataKuliah.java
+++ Pertemuan 5/MataKuliah.java
@@ -2,12 +2,12 @@
  *
  * @author Farrel Aqilla Novianto
  */
-public class MataKuliah {
+public class MataKuliah{
     String namaMK;
     int kuota;
     Dosen dosen;
 
-    public MataKuliah(String namaInput, int kuotaInput, Dosen dosenInput) {
+    public MataKuliah(String namaInput, int kuotaInput, Dosen dosenInput){
         namaMK = namaInput;
         kuota = kuotaInput;
         dosen = dosenInput;
