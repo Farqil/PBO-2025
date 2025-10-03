f74264b5123657073f38bb5a246810b42ed18600
Farqil <farrel.aqilla.nov@gmail.com>
2025-10-03 12:41:04 +0700

    tugas 6

---
+++ Pertemuan 6/Note.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author Farrel Aqilla Novianto
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Note {
+    private String title;
+    private String content;
+    public Note(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getContent(){
+        return content;
+    }
+    
+    @Override
+    public String toString(){
+        return "Judul: " + title + "\nIsi: " + content;
+    }
+}
